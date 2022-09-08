a3461fd17bf8bd66923f3dfe43ed8254484587f1
felixmaina <karani.maina2010@gmail.com>
2022-09-08 19:13:16 +0300

    create a universal response class

--- src/main/java/com/example/alex/wrappers/UniversalResponse.java
+++ src/main/java/com/example/alex/wrappers/UniversalResponse.java
@@ -1,12 +1,12 @@
-//package com.example.alex.wrappers;
-//
-//import lombok.Builder;
-//import lombok.Data;
-//
-//@Data
-//@Builder
-//public class UniversalResponse {
-//    private int status;
-//    private String message;
-//    private Object data;
-//}
+package com.example.alex.wrappers;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UniversalResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
