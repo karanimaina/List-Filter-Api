313807b995c05e716fdc7fbc694e1c7afb6e83ac
felixmaina <karani.maina2010@gmail.com>
2022-09-08 19:12:29 +0300

    add atotal dto class

--- src/main/java/com/example/alex/wrappers/Total.java
+++ src/main/java/com/example/alex/wrappers/Total.java
@@ -1,44 +1,44 @@
-//package com.example.alex.wrappers;
+package com.example.alex.wrappers;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Total {
+    @JsonProperty("next_marker")
+    private String nextMarker;
+    @JsonProperty("results")
+    private List<Result> results = null;
+    @JsonProperty("presence_active_ids")
+    private List<String> presenceActiveIds = null;
+    @JsonProperty("ok")
+    private Boolean ok;
+
+//    public String getNextMarker() {
+//        return nextMarker;
+//    }
 //
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
+//    public List<Result> getResults() {
+//        results = new ArrayList<>();
+//        return results;
+//    }
 //
-//import java.util.ArrayList;
-//import java.util.List;
+//    public List<String> getPresenceActiveIds() {
+//        return presenceActiveIds;
+//    }
 //
-//@Data
-//@JsonIgnoreProperties(ignoreUnknown = true)
-//public class Total {
-//    @JsonProperty("next_marker")
-//    private String nextMarker;
-//    @JsonProperty("results")
-//    private List<Result> results = null;
-//    @JsonProperty("presence_active_ids")
-//    private List<String> presenceActiveIds = null;
-//    @JsonProperty("ok")
-//    private Boolean ok;
-//
-////    public String getNextMarker() {
-////        return nextMarker;
-////    }
-////
-////    public List<Result> getResults() {
-////        results = new ArrayList<>();
-////        return results;
-////    }
-////
-////    public List<String> getPresenceActiveIds() {
-////        return presenceActiveIds;
-////    }
-////
-////    public Boolean getOk() {
-////        return ok;
-////    }
-//}
-//
-//
-//
-//
-//
+//    public Boolean getOk() {
+//        return ok;
+//    }
+}
+
+
+
+
+
