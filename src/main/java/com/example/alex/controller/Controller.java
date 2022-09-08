37e0a41ecf2255cb13fa01f7a7151612c22dac72
felixmaina <karani.maina2010@gmail.com>
2022-09-08 19:33:18 +0300

    add an add endpoint for adding objects

--- src/main/java/com/example/alex/controller/Controller.java
+++ src/main/java/com/example/alex/controller/Controller.java
@@ -1,13 +1,14 @@
-//package com.example.alex.controller;
-//
-//import com.example.alex.service.FilterService;
-//import com.example.alex.wrappers.UniversalResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
+package com.example.alex.controller;
+
+import com.example.alex.service.FilterService;
+import com.example.alex.wrappers.UniversalResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/selected")
@@ -17,8 +18,8 @@ public class Controller{
      ResponseEntity<UniversalResponse>getAllEntities(){
         return filterService.getAllSelected();
     }
-////    @PostMapping("/add")
-////    public ResponseEntity<UniversalResponse>  addSelected(@RequestBody Total total){
-//////        return filterService.filter(total);
-////    }
-//}
+    @PostMapping("/add")
+    public ResponseEntity<UniversalResponse>  addSelected(@RequestBody Total total){
+       return filterService.filter(total);
+    }
+}
